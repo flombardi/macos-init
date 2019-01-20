21edbb269681649aa18a0d176fd201d240c3602d
Franck Lombardi <flombardi@users.noreply.github.com>
2019-01-20 21:56:01 +0100

    remove iTerm theme

--- macos.sh
+++ macos.sh
@@ -122,9 +122,6 @@ defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 # iTerm 2                                                                     #
 ###############################################################################
 
-# Install the Solarized Dark theme for iTerm
-open "${HOME}/.dotfiles/macos-init/Solarized Dark.itermcolors"
-
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 

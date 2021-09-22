443a4c473a889b6ab7b67392e077653b1b18672b
Franck Lombardi <flombardi@users.noreply.github.com>
2021-09-22 22:49:49 +0200

    Install Nerd Font

--- brew.sh
+++ brew.sh
@@ -47,6 +47,10 @@ brew install python@3.9
 # Install vagrant
 brew install --cask vagrant
 
+# Install Nerd Font
+brew tap homebrew/cask-fonts
+brew install font-fira-code-nerd-font
+
 # Remove outdated versions from the cellar.
 brew cleanup
 
